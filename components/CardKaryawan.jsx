f6a544763b9eb1b8c7acaa5282ad06b4190da4d4
iWasKriss <forstrips74@gmail.com>
2026-09-25 16:31:43 +0700

    final

--- components/CardKaryawan.jsx
+++ components/CardKaryawan.jsx
@@ -10,8 +10,10 @@ export default function CardKaryawan({ data }) {
   console.log(data);
   return (
     // Wrapper Card Karyawan
+
     <div className="w-auto h-auto rounded-4xl flex flex-col items-center p-6 drop-shadow-lg drop-shadow-black bg-linear-to-b from-red-500 to-red-900">
       {/* Profile Picture */}
+
       <Image
         src={data.profilePicture}
         alt="pfp-1"
@@ -19,26 +21,28 @@ export default function CardKaryawan({ data }) {
         width={96}
         height={96}
       />
+
       {/* Nama */}
       <h2 className="text-lg font-bold text-center">{data.nama}</h2>
+
       {/* Biodata atau Jabatan */}
       <h3 className="text-sm text-center mbe-10">{data.biodata}</h3>
 
       {/* Links */}
       <div>
         <div
-          onClick={() => bukaWebsite(data.linkYoutube)}
-          className="w-80 h-16 border-2 border-white rounded-full flex items-center justify-center m-2 hover:scale-105 transition-transform cursor-pointer drop-shadow-lg px-4"
+          onClick={() => bukaWebsite(data.linkWebsite)}
+          className="w-100 h-16 border-2 border-white rounded-full flex items-center gap-10 m-2 hover:scale-105 transition-transform cursor-pointer drop-shadow-lg px-4"
         >
-          {data.iconYoutube}
-          <p className="m-auto">{data.textYoutube}</p>
+          {data.iconWebsite}
+          <p className="">{data.textWebsite}</p>
         </div>
         <div
-          onClick={() => bukaWebsite(data.linkTwitter)}
-          className="w-80 h-16 border-2 border-white rounded-full flex items-center justify-center m-2 hover:scale-105 transition-transform cursor-pointer drop-shadow-lg px-4"
+          onClick={() => bukaWebsite(data.linkWa)}
+          className="w-100 h-16 border-2 border-white rounded-full flex items-center justify-center m-2 hover:scale-105 transition-transform cursor-pointer drop-shadow-lg px-4"
         >
-          {data.iconTwitter}
-          <p className="m-auto">{data.textTwitter}</p>
+          {data.iconWa}
+          <p className="m-auto">{data.textWa}</p>
         </div>
       </div>
     </div>
--- data/database.js
+++ data/database.js
@@ -1,16 +1,15 @@
-import { AirVent } from "lucide-react";
-import { FaYoutube } from "react-icons/fa";
+import { FaGlobe, FaWhatsapp } from "react-icons/fa";
 
 export const dataMarketing = [
   {
     nama: "Finna Monica",
     profilePicture: "/assets/image/ini.jpg",
     biodata: "Customer Service @PT BPR Multi Sembada Dana",
-    linkYoutube: "https://youtube.com",
-    textYoutube: "Youtube",
-    iconYoutube: <AirVent className="-mr-6" size={24} />,
-    linkTwitter: "https://x.com",
-    textTwitter: "Twitter",
-    iconTwitter: <FaYoutube className="-mr-6" size={24} />,
+    linkWebsite: "https://bprsembada.co.id",
+    textWebsite: "BPR Multi Sembada Dana's Official Website",
+    iconWebsite: <FaGlobe className="-mr-6" size={26} />,
+    linkWa: "https://wa.me/6287875083272",
+    textWa: "Contact us on whatsapp",
+    iconWa: <FaWhatsapp className="-mr-6" size={30} />,
   },
 ];
